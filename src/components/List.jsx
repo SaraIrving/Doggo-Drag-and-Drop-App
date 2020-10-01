c174b1f5383c2d439a62ee62343da8c0611db88a
Sara <sarakirving@gmail.com>
2020-10-01 15:25:04 -0700

    wrap List component in the Droppable wrapper and five it an id

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Droppable } from "react-beautiful-dnd";
 import ListItem from './ListItem';
 
 export default function List (props) {
 
   return (
     <div className="dogListWrapper">
+      <Droppable droppableId={id}>
         {props.state.map(doggObj => (
-      <ListItem item={doggObj.pic} key={doggObj.id} />
-    ))}
-
+        <ListItem item={doggObj.pic} key={doggObj.id} />
+        ))} 
+      </Droppable>
     </div>
 
   )
