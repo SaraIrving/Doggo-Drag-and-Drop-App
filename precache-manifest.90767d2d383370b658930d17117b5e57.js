fed01dde02266450d5aaa15438efc51e0be439ae
Sara <sarakirving@gmail.com>
2020-10-19 12:49:27 -0700

    Updates

---
+++ precache-manifest.90767d2d383370b658930d17117b5e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526f8107ca63b397fe1bd5fb47bcefbe",
+    "url": "/Doggo-Drag-and-Drop-App/index.html"
+  },
+  {
+    "revision": "166026eedbfb23588d57",
+    "url": "/Doggo-Drag-and-Drop-App/static/css/main.6a85afed.chunk.css"
+  },
+  {
+    "revision": "c3d65efcd1cd730b91d1",
+    "url": "/Doggo-Drag-and-Drop-App/static/js/2.a923b5e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Doggo-Drag-and-Drop-App/static/js/2.a923b5e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166026eedbfb23588d57",
+    "url": "/Doggo-Drag-and-Drop-App/static/js/main.e6cd1de6.chunk.js"
+  },
+  {
+    "revision": "412ae3142140f28f4bd4",
+    "url": "/Doggo-Drag-and-Drop-App/static/js/runtime-main.021ef2be.js"
+  }
+]);
